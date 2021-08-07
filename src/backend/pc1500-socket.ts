4a7d9d97c6db5d0520fe052e739d9fc1468a3d01
Jason Hsu <jasonhsu2013@gmail.com>
2021-08-06 21:10:34 -0700

    Working ported pc1500 socket

---
+++ src/backend/pc1500-socket.ts
@@ -0,0 +1,64 @@
+export type Request = [Uint8Array, Deno.Addr];
+
+export type RequestCallback = (req: Request) => void;
+
+export interface IPC1500Socket {
+    /**
+     * Starts listening for and processing requests.
+     */
+    listen(): Promise<void>;
+
+    /**
+     * Register a function to be called when an request arrives.
+     * @param fn Callback Function.
+     */
+    register(fn: RequestCallback): void;
+}
+
+export default class PC1500Socket implements IPC1500Socket {
+    private local: Deno.NetAddr & { transport: "udp" };
+    private remote: Deno.NetAddr & { transport: "udp" };
+    private callbacks: RequestCallback[] = [];
+    private enc = new TextEncoder();
+    private dec = new TextDecoder();
+    private udpSocket: Deno.DatagramConn;
+
+    constructor(localAddr: string, localPort: number, remoteAddr: string, remotePort: number) {
+        this.local = {
+            transport: "udp",
+            port: localPort,
+            hostname: localAddr
+        };
+        this.remote = {
+            transport: "udp",
+            port: remotePort,
+            hostname: remoteAddr
+        };
+        this.udpSocket = Deno.listenDatagram(this.local);
+
+
+
+    }
+
+    async listen(): Promise<void> {
+        this.udpSocket.send(this.enc.encode("U"), this.remote);
+        for await (const req of this.udpSocket) {
+            for (const fn of this.callbacks) {
+                fn(req);
+            }
+        }
+    }
+    register(fn: RequestCallback): void {
+        this.callbacks.push(fn);
+    }
+}
+
+const dec = new TextDecoder();
+function logReqMsg(req: [Uint8Array, Deno.Addr]) {
+    console.log(dec.decode(req[0]));
+}
+
+const pcSock = new PC1500Socket("192.168.1.1", 1111, "192.168.1.4", 1111);
+pcSock.register(logReqMsg);
+pcSock.listen();
+console.log("after listen()");
